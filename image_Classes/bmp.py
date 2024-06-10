9e19c697e8702ebda0a6b358a9410088affd36ac
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-06-10 19:01:17 +0330

    first commit

---
+++ image_Classes/bmp.py
@@ -0,0 +1,12 @@
+from .imageConverter import ImageConverter
+
+class BMP(ImageConverter):
+    def __init__(self, name, width, height, colorMixValue):
+        if(self.colorMixValue < 0.0 or self.colorMixValue > 1.0):
+            raise ValueError("ERROR -> BMP: ColorMixValue must be in range [0.0, 1.0].")
+        
+        super().__init__(name, width, height)
+        self.colorMixValue = colorMixValue
+
+    def getFinalFileSize(self):
+        return self.width * self.height * self.colorMixValue
---
+++ image_Classes/png.py
@@ -0,0 +1,18 @@
+from .imageConverter import ImageConverter
+
+class PNG(ImageConverter):
+    def __init__(self, name, width, height, dpi, isInterlaced):
+        super().__init__(name, width, height)
+        if(dpi < 0):
+            raise ValueError("ERROR -> PNG: DPI Cannot be negative!")
+
+        if(isInterlaced != True or isInterlaced != False):
+            raise ValueError("ERROR -> PNG: IsInterlaced must be a boolean!")
+
+        self.dpi = dpi
+        self.isInterlaced = isInterlaced
+
+    def getFinalFileSize(self):
+        finalValue = self.width * self.height * self.dpi
+        
+    
